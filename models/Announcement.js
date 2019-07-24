f0d723fb1ebfde28dcfb4fafc423e6de21c79285
Joshua Hallee <jthallee@email.neit.edu>
2019-07-24 17:05:06 -0400

    Fixed formatting

--- models/Announcement.js
+++ models/Announcement.js
@@ -1,35 +1,13 @@
 const mongoose = require("mongoose");
 
 const announcementSchema = new mongoose.Schema({
-  classroomId: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  creationDate: {
-    type: Date,
-    default: Date.now
-  },
-  published: {
-    type: Boolean,
-    default: false
-  },
-  modified: {
-    type: Boolean,
-    default: false,
-
-    modifiedDate: {
-      type: Date,
-      default: Date.now
-    }
-  }
+  classroomId: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  creationDate: { type: Date, default: Date.now },
+  isPublished: { type: Boolean, default: false },
+  isModified: { type: Boolean, default: false },
+  modifiedDate: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Announcement", announcementSchema);
