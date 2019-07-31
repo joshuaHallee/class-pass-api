4144ff2b1a494dc160b2ac454088a4feb4892ef5
Joshua Hallee <jthallee@email.neit.edu>
2019-07-31 19:43:51 -0400

    Moved route to assignments for creating assignment

--- routes/assignment.js
+++ routes/assignment.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const verify = require("./verifyToken");
 const Assignment = require("../models/Assignment");
+const Classroom = require("../models/Classroom");
 
 router.get("/", async (req, res) => {
   try {
@@ -9,6 +10,40 @@ router.get("/", async (req, res) => {
     res.json(assignments);
   } catch (err) {
     res.json({ message: error });
+  }
+});
+
+router.put("/:classroomId", verify, async (req, res) => {
+  const classroomId = req.params.classroomId;
+  console.log(classroomId);
+
+  const assignment = new Assignment({
+    classroomId: classroomId,
+    title: req.body.title,
+    description: req.body.description,
+    dueDate: req.body.dueDate,
+    isPublished: req.body.isPublished,
+    repsonses: [],
+    viewedBy: []
+  });
+
+  try {
+    const savedAssignment = await assignment.save();
+
+    const updatedClassroomAssignment = await Classroom.updateOne(
+      { _id: { $eq: classroomId } },
+      {
+        $push: {
+          assignments: {
+            assignmentId: savedAssignment._id
+          }
+        }
+      }
+    );
+
+    res.json(updatedClassroomAssignment);
+  } catch (err) {
+    res.json(err);
   }
 });
 
