63ed13a9a634853785e90f3fb46bd6a3120b38be
Doug Rose <drrose@email.neit.edu>
2019-09-01 14:57:20 -0400

    the best port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3001;
+const port = 3000;
 require("dotenv/config");
 
 //Import Routes
