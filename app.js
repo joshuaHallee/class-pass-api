e24f8887d4f4baedc3687bea55fd2824d36fe40b
Andrew Bellucci <andrew@bellucci.tech>
2019-09-16 01:26:11 -0400

    https

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 require("dotenv/config");
+var key = fs.readFileSync(__dirname + '/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '/selfsigned.crt');
+var options = {
+    key: key,
+    cert: cert
+};
 
 //Import Routes
 const announcementRoute = require("./routes/announcement");
@@ -30,6 +38,8 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
   console.log("Connected to db");
 });
 
-app.listen(port, function() {
-  console.log(`Server is listening on port ${port}`);
+var server = https.createServer(options, app);
+
+server.listen(port, () => {
+    console.log("server starting on port : " + port)
 });
